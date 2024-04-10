471bb8b7c0f41c41e4c760fc22f69c5ee9517878
MusiJVR <mousejava102@gmail.com>
2024-04-10 22:21:05 +0400

    Update fabric.mod.json

---
+++ src/main/java/net/gardenbotanical/GardenBotanicalDataGenerator.java
@@ -0,0 +1,13 @@
+package net.gardenbotanical;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+
+public class GardenBotanicalDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
