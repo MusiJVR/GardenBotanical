6c7becc4e8cb0a63707c191d47c257012c7f61a3
Roma4ka-RedBad <cool.roma2006@gmail.com>
2024-04-10 22:22:31 +0400

    Edit init files

--- src/main/java/net/gardenbotanical/GardenBotanical.java
+++ src/main/java/net/gardenbotanical/GardenBotanical.java
@@ -1,10 +1,14 @@
 package net.gardenbotanical;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GardenBotanical implements ModInitializer {
+    public static final String MOD_ID = "gardenbotanical";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-
+        LOGGER.info("Initialize " + MOD_ID + "...");
     }
 }
--- src/main/java/net/gardenbotanical/client/GardenBotanicalClient.java
+++ src/main/java/net/gardenbotanical/client/GardenBotanicalClient.java
@@ -1,10 +1,11 @@
 package net.gardenbotanical.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.gardenbotanical.GardenBotanical;
 
 public class GardenBotanicalClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        GardenBotanical.LOGGER.info("Initialize " + GardenBotanical.MOD_ID + " client...");
     }
 }
